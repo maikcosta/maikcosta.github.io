290c6e578a6206970aaa30305256d80896278c88
maikcosta@gmail.com <Mk1311cts>
2021-10-05 10:59:02 -0300

    Pdf generate

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,10 +98,17 @@ let areaCv = document.getElementById('area-cv')
 let resumeButton = document.getElementById('resume-button')
 
 // Html2pdf options
+let opt = {
+    margin:         0,
+    filename:       'MaikCosta-Resume.pd',
+    image:          { type: 'jpeg', quality: 0.98},
+    html2canvas:    { scale: 4 },
+    jsPDF:          { format: 'a4', orientation: 'portrait'}
+}
 
 // Function to call areaCv and Html2Pdf options 
 function generateResume(){
-    html2pdf(areaCv)
+    html2pdf(areaCv, opt)
 }
 
 // When the button is clicked, it executes the three functions
@@ -112,5 +119,6 @@ resumeButton.addEventListener('click', () =>{
     // 2. The PDF is generated
     generateResume()    
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    setTimeout(removeScale,5000)
 })
 
